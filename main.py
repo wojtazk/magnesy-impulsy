2db71a76e7691f5669358fa9f2ccb6b98c866613
Wojciech Kowal <w.kowal202@gmail.com>
2025-05-16 08:54:12 +0200

    Implement 2 sine waves sum (time domain)

--- main.py
+++ main.py
@@ -21,23 +21,28 @@ class App(tk.Tk):
         right_panel = tk.Frame(self)
         right_panel.pack(side="right", fill="both", expand=True)
 
-        # --- parametry sygnału ---
-        self.fs = 1000.0  # częstotliwość próbkowania [Hz]
-        self.T_obs = 1.0  # czas obserwacji [s]
-        N = int(self.fs * self.T_obs)
-        self.t = np.linspace(0, self.T_obs, N, endpoint=False)
-
         # --- sekcja 1 ---
         # ( variable, "label" )
         self.variables = [
+            # parametry sygnału
+            (tk.StringVar(value="1000.00"), "Częstotliwość próbkowania [Hz]"),
+            (tk.StringVar(value="1.00"), "Czas obserwacji [s]"),
+
+
             # sinusoida 1
-            (tk.StringVar(value="4.0"), "Częstotliwość [Hz]:"),
-            (tk.StringVar(value="4.0"), "Test")
+            (tk.StringVar(value="1.00"), "Amplituda_1"),
+            (tk.StringVar(value="0.00"), "Faza_1 [PI]"),
+            (tk.StringVar(value="4.0"), "Częstotliwość_1 [Hz]:"),
+
+            # sinusoida 2
+            (tk.StringVar(value="1.00"), "Amplituda_2"),
+            (tk.StringVar(value="0.00"), "Faza_2 [PI]"),
+            (tk.StringVar(value="4.0"), "Częstotliwość_2 [Hz]:"),
         ]
         # dodawanie do okienka
         for var in self.variables:
-            tk.Label(left_panel, text=var[1]).pack(anchor=tk.W)
-            tk.Entry(left_panel, textvariable=var[0], width=10).pack(padx=5, pady=5)
+            tk.Label(left_panel, text=var[1]).pack(anchor="w")
+            tk.Entry(left_panel, textvariable=var[0], width=10).pack(padx=0, pady=5)
         # dodawanie listener'ów (Enter i zmiana focusu)
         for child in left_panel.winfo_children():
             for seq in ("<Return>", "<FocusOut>"):
@@ -52,22 +57,29 @@ class App(tk.Tk):
         self.update_plot()
 
     def update_plot(self, event=None):
-        # ddczyt i walidacja fs
-        text = self.variables[0][0].get().replace(",", ".")
-        try:
-            f = float(text)
-            if f <= 0:
-                raise ValueError
-        except ValueError:
-            f = 4.0
-        self.variables[0][0].set(f"{f:.2f}")
+        # ddczyt i walidacja zmiennych
 
-        # generowanie sygnału: y(t) = sin(2π * f * t)
-        y = np.sin(2 * np.pi * f * self.t)
+        fs = float(self.variables[0][0].get().replace(",", "."))
+        t_obs = float(self.variables[1][0].get().replace(",", "."))
+        a = float(self.variables[2][0].get().replace(",", "."))
+        phi = float(self.variables[3][0].get().replace(",", "."))
+        f = float(self.variables[4][0].get().replace(",", "."))
+
+        a_2 = float(self.variables[5][0].get().replace(",", "."))
+        phi_2 = float(self.variables[6][0].get().replace(",", "."))
+        f_2 = float(self.variables[7][0].get().replace(",", "."))
+
+        # punkty w czasie
+        t = np.linspace(0, t_obs, int(fs * t_obs), endpoint=False)
+
+        # generowanie sygnału 1: y(t) = A * sin(2π * f * t  + phi)
+        y = a * np.sin(2 * np.pi * f * t + phi*np.pi)
+        # generowanie sygnału 2
+        y_2 = a_2 * np.sin(2 * np.pi * f_2 * t + phi_2 * np.pi)
 
         # rysowanie
         self.ax.clear()
-        self.ax.plot(self.t, y, lw=1)
+        self.ax.plot(t, y + y_2, lw=1)
         # self.ax.set_title(f"Sinus: $f = {f:.2f}\\,$Hz")
         self.ax.set_xlabel("Czas [s]")
         self.ax.set_ylabel("Amplituda")
@@ -77,5 +89,5 @@ class App(tk.Tk):
 
 if __name__ == "__main__":
     app = App()
-    # app.geometry("900x500")
+    app.geometry("900x500")
     app.mainloop()
